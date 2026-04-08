f05ebf81f5dee1d73d1353c8e1fdc7926ad46119
Adrian Bautista <abautista@venturae.com.mx>
2026-04-08 15:02:23 -0500

    fix: modify output to static

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ const defaultLang = PUBLIC_DEFAULT_LOCALE || "en";
 
 /** @type {import('astro/config').AstroUserConfig} */
 const config = {
-  output: 'hybrid', // Changed to server for SSR support with Cloudflare
+  output: 'static', // Changed to server for SSR support with Cloudflare
   adapter: cloudflare(),
   site: PUBLIC_SITE_URL, // URL para pruebas locales.
 
