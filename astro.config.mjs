08d4e4b1315a31a3910739854102ec4e24560fa5
Adrian Bautista <abautista@venturae.com.mx>
2026-04-21 16:09:59 -0500

    update astro config v2

--- astro.config.mjs
+++ astro.config.mjs
@@ -18,7 +18,9 @@ const config = {
   output: 'static', // Changed to server for SSR support with Cloudflare
   adapter: cloudflare(),
   site: PUBLIC_SITE_URL, // URL para pruebas locales.
-
+  build: {
+    inlineStylesheets: 'always'
+  },
   image: {
     domains: ['localhost', '127.0.0.1'],
     remotePatterns: [{ protocol: "https" }],
